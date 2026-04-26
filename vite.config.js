d682b9f9c3662ee976e44def55383ea9bb221c0a
Daniel Sanchez <ingdanieljs@gmail.com>
2026-04-26 18:34:34 -0500

    [main] remove comments

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Base path para GitHub Pages: https://<usuario>.github.io/<repo>/
-  // Si despliegas en un dominio raíz (user.github.io) o dominio propio, cambia a '/'
   base: '/tic-tac-toe/',
   plugins: [react()],
 })
